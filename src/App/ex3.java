b8a44b6efc157360ade2a0ca3b1bf15b4e8d088a
VitorInc <vitordc.contato@gmail.com>
2021-03-01 11:01:23 -0300

    Terceiro exercício DONE

---
+++ src/App/ex3.java
@@ -0,0 +1,29 @@
+package App;
+
+import java.util.Scanner;
+
+public class ex3 {
+    public static void main(String[] args) {
+        System.out.println("");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o primeiro numero");
+        int primeiro = sc.nextInt();
+        System.out.println("Digite o segundo numero");
+        int segundo = sc.nextInt();
+
+        if(primeiro  >= 15){
+            System.out.println("Digite um numero menor que 15");
+        }
+        else if(segundo >= 15) {
+            System.out.println("Digite um numero menor que 15");
+        }
+        else {
+            for(int i = primeiro; primeiro > segundo; i++ ){
+                System.out.println(Math*);
+
+            }
+        }
+
+        sc.close();
+    }
+}
